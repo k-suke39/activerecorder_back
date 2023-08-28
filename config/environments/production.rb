01faf13362097f38c41db6401917767b81d0289b
k-suke39 <pz.mozarla@gmail.com>
2023-08-28 21:51:37 +0900

    hostの修正 ver5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts.clear
+  config.hosts << "activerecorder-back.onrender.com"
 end
