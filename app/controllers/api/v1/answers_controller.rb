2ed7dd64132a59d25425390de05d53e69d3700ed
k-suke39 <pz.mozarla@gmail.com>
2023-09-16 13:01:13 +0900

    ⚡️ add: action

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::AnswersController < ApplicationController
   def check
     user_answer = eval(URI.decode_www_form_component(params[:user_answer]))
-    answer = Practice.find_by(id: 1)
-    render json: { result: user_input == answer }
+    answer = Practice.find_by(id: 1).answer
+    question_answer = eval(answer)
+
+    pp user_answer == question_answer
+    render json: { result: user_answer == question_answer }
   rescue => e
     render json: { result: false }
   end
