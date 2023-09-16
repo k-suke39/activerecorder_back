58f4affd45cce24594055249a5b780727742161e
k-suke39 <pz.mozarla@gmail.com>
2023-09-16 17:15:52 +0900

    ⚡️ add: 文言の変更

--- app/controllers/api/v1/executions_controller.rb
+++ app/controllers/api/v1/executions_controller.rb
@@ -2,5 +2,7 @@ class Api::V1::ExecutionsController < ApplicationController
   def execute
     query = eval(URI.decode_www_form_component(params[:active_record_string]))
     render json: query
+  rescue => e
+    render json: { result: "実行できませんでした。コードが正しいか一度確認してみてください。" }
   end
 end
