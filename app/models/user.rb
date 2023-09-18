f908984ee3230836563ce56fdf75528e7d3f8498
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 06:20:13 +0900

    mod: バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :commented_posts, through: :likes, source: :post
 end
