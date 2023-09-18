37021e23cb6db0842b7121d5b77a225d65776e77
k-suke39 <pz.mozarla@gmail.com>
2023-09-18 11:29:38 +0900

    add: 本番環境でseed_fuを実行するように修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+if [ "$RAILS_ENV" = "production" ]; then bundle exec rake db:seed_fu; fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
