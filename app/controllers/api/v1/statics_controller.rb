cd8f7d134e75ae826882e95bed9766c4732d2c0c
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 07:13:54 +0900

    add

--- app/controllers/api/v1/statics_controller.rb
+++ app/controllers/api/v1/statics_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::StaticsController < ApplicationController
   def index
-    render json: {"response": "確認"}
+    chapter_11 = Chapter.find_by(id: 11)
+    chapter_11.delete
+    chapter_12 = Chapter.find_by(id: 12)
+    chapter_12.delete
+    chapter_13 = Chapter.find_by(id: 13)
+    chapter_13.delete
+    chapter_6 = Chapter.find_by(id: 6)
+    chapter_6.delete
+    chapter_7 = Chapter.find_by(id: 7)
+    chapter_7.delete
+    chapter_8 = Chapter.find_by(id: 8)
+    chapter_8.delete
+
+    render json: { true: true }
+    
+  rescue StandardError => e
+    render json: { error: false }
+  end
   end
 end
