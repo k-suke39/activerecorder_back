1af9d1ec4e61cc0e6f2aa35e04a6bc60a4e6d860
k-suke39 <pz.mozarla@gmail.com>
2023-09-20 01:13:47 +0900

    fix: リファクタ

--- app/controllers/api/v1/executions_controller.rb
+++ app/controllers/api/v1/executions_controller.rb
@@ -1,79 +1,82 @@
 class Api::V1::ExecutionsController < Api::V1::ApplicationController
+  before_action :decode_and_check_input, only: [:index, :check]
+
   def index
-    input_string = URI.decode_www_form_component(params[:active_record_string])
-
-    check_strings(input_string, forbidden_string)
-
-    query = eval(input_string)
-
-    render json: query
-  rescue => e
-    render json: { result: "実行できませんでした。コードが正しいか一度確認してみてください。" }
+    render json: execute_and_rescue(@input_string)
   end
 
   def check 
-    input_string = URI.decode_www_form_component(params[:user_answer])
-
-    check_strings(input_string, forbidden_string)
-
-    user_answer  = eval(input_string)
-
+    user_answer = execute_and_rescue(@input_string)
     answer = Practice.find_by(id: params[:practice_id]).answer
-    question_answer = eval(answer)
+    question_answer = execute_and_rescue(answer)
 
-    render json: { result: user_answer == question_answer }
-    
-  rescue => e
-    render json: { result: false }
+    render json: { result: user_answer[:result] == question_answer[:result] }
   end
-end
 
-private
+  private
 
-def check_strings(input, strings)
-  strings.each do |s|
-    if input.include?(s)
-      raise "実行できませんでした。コードが正しいか一度確認してみてください。"
+  def decode_and_check_input
+    @input_string = URI.decode_www_form_component(params[:active_record_string])
+    check_strings(@input_string, forbidden_string)
+  end
+
+  def execute_input(input)
+    eval(input) 
+  end
+
+  def execute_and_rescue(input)
+    begin
+      { result: execute_input(input) }
+    rescue => e
+      { result: "実行できませんでした。コードが正しいか一度確認してみてください。" }
     end
   end
-end
 
-def forbidden_string
-  return [
-    "Work",
-    "Chapter", 
-    "Practice",
-    "Authentication",
-    "build",
-    "new",
-    "touch",
-    "increment",
-    "increment!",
-    "decrement",
-    "decrement!",
-    "delete_all",
-    "save",
-    "create",
-    "create!",
-    "update",
-    "update!",
-    "update_attribute",
-    "update_attributes",
-    "update_attributes!",
-    "destroy",
-    "destroy!",
-    "destroy_all",
-    "delete",
-    "delete_all",
-    "first_or_create",
-    "first_or_create!",
-    "first_or_initialize",
-    "toggle",
-    "toggle!",
-    "destroy_by",
-    "delete_by",
-    "reverse",
-    "split",
-    "join",
-  ]
+  def check_strings(input, strings)
+    strings.each do |s|
+      if input.include?(s)
+        raise "実行できませんでした。コードが正しいか一度確認してみてください。"
+      end
+    end
+  end
+
+  def forbidden_string
+    return [
+      "Work",
+      "Chapter", 
+      "Practice",
+      "Authentication",
+      "build",
+      "new",
+      "touch",
+      "increment",
+      "increment!",
+      "decrement",
+      "decrement!",
+      "delete_all",
+      "save",
+      "create",
+      "create!",
+      "update",
+      "update!",
+      "update_attribute",
+      "update_attributes",
+      "update_attributes!",
+      "destroy",
+      "destroy!",
+      "destroy_all",
+      "delete",
+      "delete_all",
+      "first_or_create",
+      "first_or_create!",
+      "first_or_initialize",
+      "toggle",
+      "toggle!",
+      "destroy_by",
+      "delete_by",
+      "reverse",
+      "split",
+      "join",
+    ]
+  end
 end
