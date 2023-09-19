a62ecdaaca60dad7f1b279ec2c553e0b52140b83
k-suke39 <pz.mozarla@gmail.com>
2023-09-19 19:05:15 +0900

    mod

--- app/controllers/api/v1/executions_controller.rb
+++ app/controllers/api/v1/executions_controller.rb
@@ -21,7 +21,6 @@ class Api::V1::ExecutionsController < Api::V1::ApplicationController
   end
 
   def check 
-    binding.pry
     input_string = URI.decode_www_form_component(params[:user_answer])
 
     check_strings(input_string, forbidden_string)
