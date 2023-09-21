7fe2018f8bae87b1b68420b2f2811ffe8ff87542
k-suke39 <pz.mozarla@gmail.com>
2023-09-21 11:15:54 +0900

    🐛 mod: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -157,7 +157,7 @@ likes.each do |like|
 end
 
 works = [
-  { id: 1, name: 'トライアル編', description: '操作に慣れてみましょう!', slug: 'trial', order_number: 1 },
+  { id: 1, name: 'トライアル編', description: '操作に慣れてみよう!', slug: 'trial', order_number: 1 },
   { id: 2, name: '初級編', description: 'current_userを使ってデータを取得してみよう!', slug: 'basic', order_number: 1 },
   { id: 3, name: '中級編', description: 'current_userを使って複雑なデータを取得してみよう!', slug: 'intermediate',
     order_number: 1 }
