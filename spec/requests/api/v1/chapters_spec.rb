f1b40233745be29ef0022b23f4959f4dcf2b672d
k-suke39 <pz.mozarla@gmail.com>
2023-12-10 17:09:54 +0900

    [fix]lint

--- spec/requests/api/v1/chapters_spec.rb
+++ spec/requests/api/v1/chapters_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::ChaptersController, type: :controller do
   describe 'GET #index' do
     let!(:user) { create(:user) }
     let!(:work) { create(:work) }
-    let!(:chapters) { create(:chapter,  work:) }
+    let!(:chapters) { create(:chapter, work:) }
 
     before do
       get :index, params: { slug: work.slug }
@@ -19,18 +19,18 @@ RSpec.describe Api::V1::ChaptersController, type: :controller do
     it 'JSON body response contains expected work practices' do
       json_response = JSON.parse(response.body)
       expect(json_response).to match({
-        "chapters" => [
+        'chapters' => [
           {
-            "id" => chapters.id,
-            "name" => chapters.name,
-            "slug" => chapters.slug,
-            "order_number" => chapters.order_number,
-            "created_at" => chapters.created_at.as_json,
-            "updated_at" => chapters.updated_at.as_json,
-            "work_id" => chapters.work_id
+            'id' => chapters.id,
+            'name' => chapters.name,
+            'slug' => chapters.slug,
+            'order_number' => chapters.order_number,
+            'created_at' => chapters.created_at.as_json,
+            'updated_at' => chapters.updated_at.as_json,
+            'work_id' => chapters.work_id
           }
         ],
-        "description" => work.description
+        'description' => work.description
       }.as_json)
     end
   end
